a93fe2554eff045fa5f2adda87d028b26cbd1bab
ipekmuhammet <muhammetipek57@hotmail.com>
2020-05-20 09:42:40 +0300

    init contact page

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+class Home extends React.Component {
+    render() {
+        return (
+            <>
+                <Navbar />
+                <Footer />
+            </>
+        )
+    }
+}
+
+export default Home
