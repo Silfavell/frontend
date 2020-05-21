750a91821aca031d54a439e053654d1e00e42fc0
ipekmuhammet <muhammetipek57@hotmail.com>
2020-05-21 14:59:12 +0300

    product interactions

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { IoMdHeartEmpty, IoIosEye } from "react-icons/io"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import prod1 from '../style/images/prod_1.png'
+import './Product.css'
 
 class Product extends React.Component {
     render() {
@@ -12,20 +13,35 @@ class Product extends React.Component {
         } = this.props.item
 
         return (
-            <div className='col-md-3 ml-auto d-relative'>
-                <img
-                    src='http://silfavell.com/img/product/4.jpg'
-                    alt=''
-                    className='w-100' />
+            <div className='col-md-3 ml-auto d-relative product'>
+                <div className='position-relative interface-container'>
+                    <img
+                        src='http://silfavell.com/img/product/4.jpg'
+                        alt=''
+                        className='w-100' />
+
+                    <div className='interface'>
+                        <div className="top col-md-12">
+                            <div className="col-md-6 d-flex align-items-center text-white add-to-favorite">
+                                <IoMdHeartEmpty size={24} />
+                            </div>
+                        </div>
+
+                        <div className="bottom col-md-12">
+                            <div className="col-md-6 d-flex align-items-center justify-content-center text-white inspect">Incele</div>
+                            <div className="col-md-6 d-flex align-items-center justify-content-center text-white add-to-cart">Sepete Ekle</div>
+                        </div>
+                    </div>
+                </div>
 
                 <div class='mb-3'>
-                    <div className='col-md-12  d-flex align-items-center justify-content-center'>
-                        <div class='h4 p-2 text-black font-weight-normal'>
+                    <div className='col-md-12 d-flex align-items-center justify-content-center'>
+                        <div class='h5 p-3 text-black font-weight-normal'>
                             {'₺' + price.toFixed(2).toString().replace('.', ',')}
                         </div>
                     </div>
                     <div className='col-md-12 d-flex flex-row justify-content-center align-items-center' style={{ textAlign: 'center' }}>
-                        <div class='p-2 text-black font-weight-normal'>
+                        <div class='h6 text-black font-weight-normal'>
                             {name}
                         </div>
                     </div>
