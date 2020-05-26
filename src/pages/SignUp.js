726662adf1850b79741e3ca9b6e032dd2803ccc9
ipekmuhammet <muhammetipek57@hotmail.com>
2020-05-26 09:43:42 +0300

    sign up init

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
+import axios from 'axios'
+import $ from 'jquery'
+import Cookies from 'universal-cookie'
 
 import '../style/fonts/icomoon/style.css'
 import '../style/css/googleMukta.css'
@@ -9,79 +12,226 @@ import '../style/css/style.css'
 
 import SiteWrap from '../components/SiteWrap'
 
+const cookies = new Cookies()
+
 class SignUp extends React.Component {
+
+    state = {
+        phoneNumber: '',
+        nameSurname: '',
+        email: '',
+        password: '',
+        rePassword: '',
+        activationCode: ''
+    }
+
+    onPhoneNumberChange = (event) => {
+        this.setState({ phoneNumber: event.target.value })
+    }
+
+    onNameSurnameChange = (event) => {
+        this.setState({ nameSurname: event.target.value })
+    }
+
+    onEmailChange = (event) => {
+        this.setState({ email: event.target.value })
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({ password: event.target.value })
+    }
+
+    onRePasswordChange = (event) => {
+        this.setState({ rePassword: event.target.value })
+    }
+
+    onActivationCodeChange = (event) => {
+        this.setState({ activationCode: event.target.value })
+    }
+
+    onSignUpClick = () => {
+        const url = `${process.env.REACT_APP_API_URL}/register`
+
+        axios.post(url, this.state).then(({ status, data }) => {
+            if (status === 200) {
+                cookies.set('token', data.token)
+                cookies.set('user', JSON.stringify(data.user))
+                this.props.history.push('/')
+            }
+        })
+    }
+
+    sendActivationCode = () => {
+        if (this.state.password !== this.state.rePassword) {
+            alert('Yeni şifreniz tekrarı ile eşleşmemektedir')
+        } else {
+            const url = `${process.env.REACT_APP_API_URL}/send-activation-code`
+
+            axios.post(url, {
+                phoneNumber: this.state.phoneNumber,
+                activationCodeType: 0
+            }).then(({ status, data }) => {
+                if (status === 202) {
+                    $('#register').hide()
+                    $('#activation').fadeIn('slow')
+                }
+            }).catch((err) => {
+                alert(err.response.data)
+            })
+        }
+    }
+
+    renderSignUpScreen = () => {
+        const {
+            phoneNumber,
+            nameSurname,
+            email,
+            password,
+            rePassword
+        } = this.state
+
+        return (
+            <div id={'register'} className='p-3 p-lg-5'>
+
+                <div className='form-group row'>
+                    <div className='col-md-12'>
+                        <label htmlFor='nameSurname' className='text-black'>Name Surname <span className='text-danger'>*</span></label>
+                        <input
+                            onChange={this.onNameSurnameChange}
+                            type='name'
+                            className='form-control'
+                            id='nameSurname'
+                            name='nameSurname'
+                            placeholder='Adınız ve Soyadınızı giriniz'
+                            value={nameSurname} />
+                    </div>
+                </div>
+
+                <div className='form-group row'>
+                    <div className='col-md-12'>
+                        <label htmlFor='c_email' className='text-black'>E-Mail <span className='text-danger'>*</span></label>
+                        <input
+                            onChange={this.onEmailChange}
+                            type='email'
+                            className='form-control'
+                            id='c_email'
+                            name='c_email'
+                            placeholder='E-Posta adresinizi giriniz'
+                            value={email} />
+                    </div>
+                </div>
+
+                <div className='form-group row'>
+                    <div className='col-md-12'>
+                        <label htmlFor='password' className='text-black'>Password <span className='text-danger'>*</span></label>
+                        <input
+                            onChange={this.onPasswordChange}
+                            type='password'
+                            className='form-control'
+                            id='password'
+                            name='password'
+                            placeholder='Şifrenizi giriniz'
+                            value={password} />
+                    </div>
+                </div>
+
+                <div className='form-group row'>
+                    <div className='col-md-12'>
+                        <label htmlFor='repassword' className='text-black'>Password (repeat) <span className='text-danger'>*</span></label>
+                        <input
+                            onChange={this.onRePasswordChange}
+
+                            type='password'
+                            className='form-control'
+                            id='repassword'
+                            name='repassword'
+                            placeholder='Şifrenizi yeniden giriniz'
+                            value={rePassword} />
+                    </div>
+                </div>
+
+                <div className='form-group row'>
+                    <div className='col-md-12'>
+                        <label htmlFor='phone' className='text-black'>Phone Number <span className='text-danger'>*</span></label>
+                        <input
+                            onChange={this.onPhoneNumberChange}
+                            type='phone'
+                            className='form-control'
+                            id='phone'
+                            name='phone'
+                            placeholder='Telefon Numaranızı giriniz'
+                            value={phoneNumber} />
+                    </div>
+                </div>
+
+                <div className='form-group row'>
+                    <div className='col-md-12'>
+                        <input type='checkbox' className='form-check-label' id='dont-forget' name='dont-forget' placeholder='' />
+                        <label htmlFor='dont-forget' className='form-check-label ml-2'>Üyelik Sözleşmesi şartlarını okudum ve kabul ediyorum.</label>
+                    </div>
+                </div>
+
+                <div className='form-group row'>
+                    <div className='col-md-12 d-flex align-items-md-start justify-content-md-start'>
+                        <input type='checkbox' className='form-check-label' id='dont-forget' name='dont-forget' placeholder='' />
+                        <label htmlFor='dont-forget' className='form-check-label ml-2'>Tarafımla pazarlama ve tanıtım amaçlı iletişime geçilmesine izin veriyorum.</label>
+                    </div>
+                </div>
+
+                <div className='form-group row'>
+                    <div className='col-lg-12'>
+                        <button className='btn btn-primary btn-lg btn-block' onClick={this.sendActivationCode}>Üye Ol</button>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+
+    renderActivationScreen = () => {
+
+        const {
+            activationCode
+        } = this.state
+
+        return (
+            <div id={'activation'} className='p-3 p-lg-5' style={{ display: 'none' }}>
+
+                <div className='form-group row'>
+                    <div className='col-md-12'>
+                        <label htmlFor='phone' className='text-black'>Aktivasyon Kodu <span className='text-danger'>*</span></label>
+                        <input
+                            onChange={this.onActivationCodeChange}
+                            type='phone'
+                            className='form-control'
+                            id='phone'
+                            name='phone'
+                            placeholder='Aktivasyon kodunuzu giriniz'
+                            value={activationCode} />
+                    </div>
+                </div>
+
+                <div className='form-group row'>
+                    <div className='col-lg-12'>
+                        <button className='btn btn-primary btn-lg btn-block' onClick={this.onSignUpClick}>Üye Ol</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <SiteWrap divider>
                 <div className='container'>
                     <div className="col-md-12 d-flex align-items-center justify-content-center">
                         <div className='col-md-6'>
-                            <form action='#' method='post'>
-                                <div className='p-3 p-lg-5'>
-
-                                    <div className='form-group row'>
-                                        <div className='col-md-12'>
-                                            <label htmlFor='nameSurname' className='text-black'>Name Surname <span className='text-danger'>*</span></label>
-                                            <input
-                                                onChange={this.onNameSurnameChange}
-                                                type='name'
-                                                className='form-control'
-                                                id='nameSurname'
-                                                name='nameSurname'
-                                                placeholder='Adınız ve Soyadınızı giriniz' />
-                                        </div>
-                                    </div>
-
-                                    <div className='form-group row'>
-                                        <div className='col-md-12'>
-                                            <label htmlFor='c_email' className='text-black'>E-Mail <span className='text-danger'>*</span></label>
-                                            <input type='email' className='form-control' id='c_email' name='c_email' placeholder='E-Posta adresinizi giriniz' />
-                                        </div>
-                                    </div>
-
-                                    <div className='form-group row'>
-                                        <div className='col-md-12'>
-                                            <label htmlFor='password' className='text-black'>Password <span className='text-danger'>*</span></label>
-                                            <input type='password' className='form-control' id='password' name='password' placeholder='Şifrenizi giriniz' />
-                                        </div>
-                                    </div>
-
-                                    <div className='form-group row'>
-                                        <div className='col-md-12'>
-                                            <label htmlFor='repassword' className='text-black'>Password (repeat) <span className='text-danger'>*</span></label>
-                                            <input type='password' className='form-control' id='repassword' name='repassword' placeholder='Şifrenizi yeniden giriniz' />
-                                        </div>
-                                    </div>
-
-                                    <div className='form-group row'>
-                                        <div className='col-md-12'>
-                                            <label htmlFor='phone' className='text-black'>Phone Number <span className='text-danger'>*</span></label>
-                                            <input type='phone' className='form-control' id='phone' name='phone' placeholder='Telefon Numaranızı giriniz' />
-                                        </div>
-                                    </div>
-
-                                    <div className='form-group row'>
-                                        <div className='col-md-12'>
-                                            <input type='checkbox' className='form-check-label' id='dont-forget' name='dont-forget' placeholder='' />
-                                            <label htmlFor='dont-forget' className='form-check-label ml-2'>Üyelik Sözleşmesi şartlarını okudum ve kabul ediyorum.</label>
-                                        </div>
-                                    </div>
-
-                                    <div className='form-group row'>
-                                        <div className='col-md-12 d-flex align-items-md-start justify-content-md-start'>
-                                            <input type='checkbox' className='form-check-label' id='dont-forget' name='dont-forget' placeholder='' />
-                                            <label htmlFor='dont-forget' className='form-check-label ml-2'>Tarafımla pazarlama ve tanıtım amaçlı iletişime geçilmesine izin veriyorum.</label>
-                                        </div>
-                                    </div>
-
-                                    <div className='form-group row'>
-                                        <div className='col-lg-12'>
-                                            <input type='submit' className='btn btn-primary btn-lg btn-block' value='Üye Ol' />
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </form>
+                            {
+                                this.renderSignUpScreen()
+                            }
+                            {
+                                this.renderActivationScreen()
+                            }
                         </div>
                     </div>
                 </div>
