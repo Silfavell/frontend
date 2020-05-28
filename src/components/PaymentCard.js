d0e89c517d64c3e745a4d9ba8b7e88a9acddc987
ipekmuhammet <muhammetipek57@hotmail.com>
2020-05-28 14:30:55 +0300

    list cards

---
+++ src/components/PaymentCard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class PaymentCard extends React.Component {
+
+    onClick = () => {
+        console.log('set selected card')
+        // this.props.setSelectedCard(this.props.item.)
+    }
+
+    render() {
+        return (
+            <div className='col-md-12 mb-4' style={{
+                border: this.props.selected ? '3px solid #80DF80' : '1px solid #dee2e6'
+            }} onClick={this.onClick}>
+                <div className='row p-4'>
+                    <div className='col-md-12'>
+                        <label className='text-black float-md-left font-weight-bold'>{this.props.item.cardAlias}</label>
+
+                        <span style={{ fontWeight: 'normal' }} className={'pl-4'}>{`**** **** **** ${this.props.item.lastFourDigits}`}</span>
+
+                        <span style={{ fontWeight: 'normal' }} className={'pl-4'}>
+                            <img alt='' src={'https://n11scdn.akamaized.net/static/new-design/static/img/layout/mastercard-logo.png?v=1590583905836'} />
+                        </span>
+
+                        <span className='text-primary float-md-right' style={{ fontWeight: 'bold', cursor: 'pointer' }}>Sil</span>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default PaymentCard
--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import axios from 'axios'
 import Cookies from 'universal-cookie'
+import $ from 'jquery'
 
 import SiteWrap from '../components/SiteWrap'
 import EmptyAddressCart from '../components/EmptyAddressCart'
 import AddressCart from '../components/AddressCart'
 import AddressPopup from '../components/AddressPopup'
+import PaymentCard from '../components/PaymentCard'
 
 const cookies = new Cookies()
 
@@ -14,7 +16,47 @@ class Payment extends React.Component {
     state = {
         products: [],
         addresses: [],
-        showSaveAddressPopup: false
+        cards: [],
+        selectedAddress: 0,
+        showSaveAddressPopup: false,
+        showNewCardSection: false,
+
+        cardAlias: 'Alias',
+        cardHolderName: 'Holder',
+        cardNumber: '5526080000000006',
+        expireYear: '09',
+        expireMonth: '2023',
+        cvc2: '555'
+    }
+
+    saveCard = () => {
+        const {
+            cardAlias,
+            cardNumber,
+            cardHolderName,
+            expireMonth,
+            expireYear,
+            cvc2
+        } = this.state
+
+        axios.post(`${process.env.REACT_APP_API_URL}/user/payment-card`, {
+            card: {
+                cardAlias,
+                cardHolderName,
+                cardNumber,
+                expireMonth,
+                expireYear,
+                // cvc2
+            }
+        }).then((res) => {
+            console.log(res)
+        }).catch((err) => {
+            alert(err.response.data.error)
+        })
+    }
+
+    showNewCardSection = (event) => {
+        this.setState({ showNewCardSection: event.target.checked })
     }
 
     showSaveAddressPopup = () => {
@@ -26,15 +68,215 @@ class Payment extends React.Component {
     }
 
     UNSAFE_componentWillMount() {
-        axios.get(`${process.env.REACT_APP_API_URL}/user/cart`).then(({ data }) => {
-            if (data) {
-                this.setState({ products: Object.values(data), addresses: cookies.get('user').addresses })
+        axios.get(`${process.env.REACT_APP_API_URL}/user/cart`).then(({ data: products }) => {
+            if (products) {
+                axios.get(`${process.env.REACT_APP_API_URL}/user/list-cards`).then(({ data }) => {
+                    this.setState({
+                        products: Object.values(products),
+                        addresses: cookies.get('user').addresses,
+                        cards: data.cardDetails
+                    })
+                })
             }
         })
     }
 
     onCompletePaymentClick = () => {
         console.log('complete payment')
+        this.saveCard()
+    }
+
+    onAddressOptionsClick = () => {
+        $('#paymentOptions').hide()
+        $('#addresses').fadeIn('slow')
+    }
+
+    onPaymentOptionsClick = () => {
+        $('#addresses').hide()
+        $('#paymentOptions').fadeIn('slow')
+    }
+
+    onAliasChange = (event) => {
+        this.setState({ cardAlias: event.target.value })
+    }
+
+    onCardHolderNameChange = (event) => {
+        this.setState({ cardHolderName: event.target.value })
+    }
+
+    onCardNoChange = (event) => {
+        this.setState({ cardNumber: event.target.value })
+    }
+
+    onExpireMonthChange = (event) => {
+        this.setState({ expireMonth: event.target.value })
+    }
+
+    onExpireYearChange = (event) => {
+        this.setState({ expireYear: event.target.value })
+    }
+
+    onCvcChange = (event) => {
+        this.setState({ cvc2: event.target.value })
+    }
+
+    renderAddresses = () => (
+        <div id={'addresses'} className='row mb-5 border'>
+            <div className='col-md-12 p-4'>
+                <h3 className={'text-secondary'}>Adres Seçimi</h3>
+            </div>
+            <div className='col-md-12 p-4'>
+                <div className='row'>
+                    {
+                        this.state.addresses.map((address) => (
+                            <AddressCart item={address} />
+                        ))
+                    }
+                    <EmptyAddressCart showSaveAddressPopup={this.showSaveAddressPopup} />
+                </div>
+            </div>
+        </div>
+    )
+
+    renderPaymentOptions = () => {
+        const {
+            cardAlias,
+            cardNumber,
+            cardHolderName,
+            expireMonth,
+            expireYear,
+            cvc2
+        } = this.state
+
+        return (
+            <div id={'paymentOptions'} className='row mb-5 border' style={{ display: 'none' }}>
+                <div className='col-md-12 p-4'>
+                    <p className={'text-gray h6'}>Kredi kartı bilgileriniz Silfavell tarafından saklanmamaktadır.</p>
+                    <p className={'text-black font-weight-bold h6'}>Ödeme altyapısı MasterPass tarafından sağlanmaktadır.</p>
+                </div>
+
+                <div className='col-md-12 p-4'>
+
+                    <div className='form-group row'>
+                        {
+                            this.state.cards.map((card) => (
+                                <PaymentCard item={card} />
+                            ))
+                        }
+                    </div>
+
+                    <div className='form-group row'>
+                        <div className='col-md-12'>
+                            <input
+                                onChange={this.showNewCardSection}
+                                type='checkbox'
+                                className='form-check-label'
+                                id='dont-forget'
+                                name='dont-forget'
+                                placeholder=''
+                                style={{ cursor: 'pointer' }} />
+
+                            <label
+                                htmlFor='dont-forget'
+                                className='form-check-label ml-2 text-primary'
+                                style={{ cursor: 'pointer' }}>Yeni Bir Kredi Kartı Bilgisi Girerek Öde.</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div id={'new-card'} className='col-md-12 p-4' style={{ display: this.state.showNewCardSection ? 'unset' : 'none' }}>
+                    <div className='row'>
+                        <div className='col-md-12'>
+                            <div className='row'>
+                                <div className='col-md-12'>
+
+                                    <div className='form-group row'>
+                                        <div className='col-md-12'>
+                                            <label htmlFor='cardAlias' className='text-black'>Card Alias <span className='text-danger'>*</span></label>
+                                            <input
+                                                onChange={this.onAliasChange}
+                                                type='text'
+                                                className='form-control'
+                                                id='cardAlias'
+                                                name='cardAlias'
+                                                value={cardAlias}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className='form-group row'>
+                                        <div className='col-md-12'>
+                                            <label htmlFor='cardHolderName' className='text-black'>Kart üzerindeki isim <span className='text-danger'>*</span></label>
+                                            <input
+                                                onChange={this.onCardHolderNameChange}
+                                                type='text'
+                                                className='form-control'
+                                                id='cardHolderName'
+                                                name='cardHolderName'
+                                                value={cardHolderName}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className='form-group row'>
+                                        <div className='col-md-12'>
+                                            <label htmlFor='cardNumber' className='text-black'>Kart No <span className='text-danger'>*</span></label>
+                                            <input
+                                                onChange={this.onCardNoChange}
+                                                type='text'
+                                                className='form-control'
+                                                id='cardNumber'
+                                                name='cardNumber'
+                                                value={cardNumber}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className='form-group row'>
+                                        <div className='col-md-6'>
+                                            <label htmlFor='expireMonth' className='text-black'>Expire Month <span className='text-danger'>*</span></label>
+                                            <input
+                                                onChange={this.onExpireMonthChange}
+                                                type='text'
+                                                className='form-control'
+                                                id='expireMonth'
+                                                name='expireMonth'
+                                                value={expireMonth}
+                                            />
+                                        </div>
+                                        <div className='col-md-6'>
+                                            <label htmlFor='expireYear' className='text-black'>Expire Year <span className='text-danger'>*</span></label>
+                                            <input
+                                                onChange={this.onExpireYearChange}
+                                                type='text'
+                                                className='form-control'
+                                                id='expireYear'
+                                                name='expireYear'
+                                                value={expireYear}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className='form-group row'>
+                                        <div className='col-md-12'>
+                                            <label htmlFor='cvc2' className='text-black'>CVC2 <span className='text-danger'>*</span></label>
+                                            <input
+                                                onChange={this.onCvcChange}
+                                                type='text'
+                                                className='form-control'
+                                                id='cvc2'
+                                                name='cvc2'
+                                                value={cvc2}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 
     render() {
@@ -46,81 +288,56 @@ class Payment extends React.Component {
                     this.state.showSaveAddressPopup && <AddressPopup hideSaveAddressPopup={this.hideSaveAddressPopup} />
                 }
                 <div className='container'>
-                    <div className='row mb-5'>
-                        <div className='col-md-12'>
-                            <div className='border'></div>
-                        </div>
-                    </div>
-
-                    <div className='row mb-5 border'>
-                        <div className='col-md-12 p-4'>
-                            <h2 className={'text-secondary'}>Adres Seçimi</h2>
-                        </div>
-                        <div className='col-md-12 p-4'>
-                            <div className='row'>
-                                {
-                                    this.state.addresses.map((address) => (
-                                        <AddressCart item={address} />
-                                    ))
-                                }
-                                <EmptyAddressCart showSaveAddressPopup={this.showSaveAddressPopup} />
-                            </div>
-                        </div>
-                    </div>
-
                     <div className='row'>
-                        <div className='col-md-6'>
+                        <div className='col-md-9'>
                             <div className='row mb-5'>
-                                <div className='col-md-6 mb-3 mb-md-0'>
-                                    <button className='btn btn-primary btn-sm btn-block'>Update Cart</button>
-                                </div>
-                                <div className='col-md-6'>
-                                    <a href='/shop'>
-                                        <button className='btn btn-outline-primary btn-sm btn-block'>Continue Shopping</button>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className='row'>
                                 <div className='col-md-12'>
-                                    <label className='text-black h4' htmlFor='coupon'>Coupon</label>
-                                    <p>Enter your coupon code if you have one.</p>
-                                </div>
-                                <div className='col-md-8 mb-3 mb-md-0'>
-                                    <input type='text' className='form-control py-3' id='coupon' placeholder='Coupon Code' />
-                                </div>
-                                <div className='col-md-4'>
-                                    <button className='btn btn-primary btn-sm px-4'>Apply Coupon</button>
+                                    <div className='row border'>
+                                        <div className='col-md-6 border-right p-3' style={{ cursor: 'pointer' }} onClick={this.onAddressOptionsClick}>
+                                            <h3 className={'text-secondary'}>Adres Bilgileri</h3>
+                                            <p className={'text-primary h5'}>{this.state.addresses[this.state.selectedAddress]?.addressTitle}</p>
+                                            <p className={'text-black h6'}>{this.state.addresses[this.state.selectedAddress]?.openAddress}</p>
+                                        </div>
+                                        <div className='col-md-6 p-3' style={{ cursor: 'pointer' }} onClick={this.onPaymentOptionsClick}>
+                                            <h3 className={'text-secondary'}>Ödeme Seçenekleri</h3>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className='col-md-6 pl-5'>
-                            <div className='row justify-content-end'>
-                                <div className='col-md-7'>
-                                    <div className='row'>
-                                        <div className='col-md-12 text-right border-bottom mb-5'>
-                                            <h3 className='text-black h4 text-uppercase'>Cart Totals</h3>
-                                        </div>
-                                    </div>
-                                    <div className='row mb-5'>
-                                        <div className='col-md-6'>
-                                            <span className='text-black'>Total</span>
-                                        </div>
-                                        <div className='col-md-6 text-right'>
-                                            <strong className='text-black'>{`₺${totalPrice}`}</strong>
-                                        </div>
-                                    </div>
 
-                                    <div className='row'>
-                                        <div className='col-md-12'>
-                                            <button className='btn btn-primary btn-lg btn-block' onClick={this.onCompletePaymentClick}>Ödemeye Geç</button>
-                                        </div>
+                            {
+                                this.renderPaymentOptions()
+                            }
+                            {
+                                this.renderAddresses()
+                            }
+                        </div>
+                        <div className='col-md-3'>
+                            <div className='col-md-12 border p-4'>
+                                <div className='row'>
+                                    <div className='col-md-12 text-left mb-5'>
+                                        <h3 className='text-black h4 text-uppercase'>Cart Totals</h3>
                                     </div>
+                                </div>
+                                <div className='row mb-5'>
+                                    <div className='col-md-6'>
+                                        <span className='text-black'>Total</span>
+                                    </div>
+                                    <div className='col-md-6 text-right'>
+                                        <strong className='text-black'>{`₺${totalPrice}`}</strong>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='row pt-3'>
+                                <div className='col-md-12'>
+                                    <button className='btn btn-primary btn-lg btn-block' onClick={this.onCompletePaymentClick}>Ödemeye Geç</button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </SiteWrap >
+            </SiteWrap>
         )
     }
 }
