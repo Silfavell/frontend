8b5ea029b36759978404d051525190e926e28474
ipekmuhammet <muhammetipek57@hotmail.com>
2020-05-30 19:09:18 +0300

    remove filter by price

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -147,12 +147,6 @@ class Shop extends React.Component {
 
                                 <div className='border p-4 rounded mb-4'>
                                     <div className='mb-4'>
-                                        <h3 className='mb-3 h6 text-uppercase text-black d-block'>Filter by Price</h3>
-                                        <div id='slider-range' className='border-primary'></div>
-                                        <input type='text' name='text' id='amount' className='form-control border-0 pl-0 bg-white' disabled='' />
-                                    </div>
-
-                                    <div className='mb-4'>
                                         <h3 className='mb-3 h6 text-uppercase text-black d-block'>Size</h3>
                                         <label htmlFor='s_sm' className='d-flex'>
                                             <input type='checkbox' id='s_sm' className='mr-2 mt-1' /> <span className='text-black'>Small (2,319)</span>
