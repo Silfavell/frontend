26908b922b5b574d22737c4ab8553c46c59f95a4
ipekmuhammet <muhammetipek57@hotmail.com>
2020-07-24 17:31:06 +0300

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,7 @@ function Footer() {
                 <div className='row'>
                     <div className='col-md-6 col-lg-3 mb-4 mb-lg-0'>
                         <h3 className='footer-heading mb-4' style={{ letterSpacing: '.1em' }}>Promo</h3>
-                        <a href='#' className='block-6'>
+                        <a href='' className='block-6'>
                             <img src={about1} alt='Image placeholder' className='img-fluid rounded mb-4' />
                             <h3 className='font-weight-light  mb-0'>Finding Your Perfect Shirts This Summer</h3>
                             <p>Promo from  July 15 &mdash; 25, 2019</p>
