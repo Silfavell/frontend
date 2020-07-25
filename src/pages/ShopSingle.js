c39875a92b8efdd97c30726bfb620b3ee06ff312
ipekmuhammet <muhammetipek57@hotmail.com>
2020-07-25 11:53:14 +0300

    extra product details

--- src/pages/ShopSingle.js
+++ src/pages/ShopSingle.js
@@ -77,6 +77,17 @@ class ShopSingle extends React.Component {
         ))
     }
 
+    renderDetailRow = ({ title, value, first }) => (
+        <div className={`col-md-12 ${!first ? 'border-top' : ''}`}>
+            <div className='row'>
+                <div className='col-3 py-2 bg-light'>{title}</div>
+                <div className='border-left col-9 py-2'>
+                    <strong className='text-black'>{value}</strong>
+                </div>
+            </div>
+        </div>
+    )
+
     renderContent = ({ categories, onIncreaseClick }) => {
         const {
             _id,
@@ -110,7 +121,6 @@ class ShopSingle extends React.Component {
                 <div className='row border-bottom'>
                     <div className='col-md-6 pb-5'>
                         <ProductImages images={this.getImages(image, imageCount)} />
-
                     </div>
                     <div className='col-md-6 pb-4'>
                         <h2 className='text-black'>{name}</h2>
@@ -212,6 +222,26 @@ class ShopSingle extends React.Component {
                         <span style={{ whiteSpace: 'break-spaces' }}>
                             {details ?? 'Ürün detayı bulunmamaktadır'}
                         </span>
+                    </div>
+
+                    <div className='col-md-12'>
+                        <div className='border mt-5'>
+                            {
+                                this.renderDetailRow({ title: 'Ürün Kodu', value: '10011395', first: true })
+                            }
+                            {
+                                this.renderDetailRow({ title: 'Renk Tonu', value: 'Yeşil' })
+                            }
+                            {
+                                this.renderDetailRow({ title: 'Fayda/İhtiyaç', value: 'Sıkılaştırıcı' })
+                            }
+                            {
+                                this.renderDetailRow({ title: 'Bitiş', value: 'Mat' })
+                            }
+                            {
+                                this.renderDetailRow({ title: 'Çeşit', value: 'Tekli' })
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
