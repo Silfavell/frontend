23f7b5371f834e2d5a0a1f145790d9837daf6055
ipekmuhammet <muhammetipek57@hotmail.com>
2020-08-29 18:17:53 +0300

    ürün bulunmamaktadır center

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -404,7 +404,7 @@ class Shop extends React.Component {
 
     nonProducts = () => (
         <div className='container d-flex align-items-center justify-content-center flex-column' style={{ height: 300 }}>
-            <h2 className={'text-black'}>Ürün bulunmamaktadır</h2>
+            <h2 className={'text-black'} style={{ textAlign: 'center' }}>Ürün bulunmamaktadır</h2>
             <p className={'text-black'} style={{ fontSize: 18, marginTop: 32, textAlign: 'center' }}>Seçtiğiniz filtrelere uygun ürün bulunmamaktadır</p>
         </div>
     )
