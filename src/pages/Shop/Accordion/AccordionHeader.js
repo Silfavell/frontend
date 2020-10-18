e9bd6e82b02e530888a0077439a642bbb70347de
ipekmuhammet <muhammetipek57@hotmail.com>
2020-10-18 18:09:23 +0300

    ShopContent components

---
+++ src/pages/Shop/Accordion/AccordionHeader.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class AccordionHeader extends React.PureComponent {
+  render() {
+    return (
+      <div className='card-header p-0 bg-white' id={`heading${this.props.id}`}>
+        <h5 className='mb-0'>
+          <button
+            className='btn btn-link w-100 mx-3'
+            style={{ textAlign: 'left', color: 'black', textDecoration: 'none' }}
+            data-toggle='collapse'
+            data-target={`#collapse${this.props.id}`}
+            aria-expanded='true'
+            aria-controls={`collapse${this.props.id}`}>
+            {this.props.title}
+          </button>
+        </h5>
+      </div>
+    )
+  }
+}
+
+export default AccordionHeader
--- src/pages/Shop/ShopContent.js
+++ src/pages/Shop/ShopContent.js
@@ -6,6 +6,8 @@ import ShopProduct from '../../components/Product/ShopProduct'
 import Slider from './Slider'
 import Specifications from './Specifications'
 import ShopHeader from './ShopHeader'
+import AccordionHeader from './Accordion/AccordionHeader'
+import AccordionContent from './Accordion/AccordionContent'
 
 import {
   getPageText,
@@ -164,52 +166,20 @@ class ShopContent extends React.PureComponent {
             }
 
             <div className='card mb-3'>
-              <div className='card-header p-0 bg-white' id={`heading${'brands'}`}>
-                <h5 className='mb-0'>
-                  <button
-                    className='btn btn-link w-100 mx-3'
-                    style={{ textAlign: 'left', color: 'black', textDecoration: 'none' }}
-                    data-toggle='collapse'
-                    data-target={`#collapse${'brands'}`}
-                    aria-expanded='true'
-                    aria-controls={`collapse${'brands'}`}>
-                    {'Markalar'}
-                  </button>
-                </h5>
-              </div>
+              <AccordionHeader
+                title={'Markalar'}
+                id={'brands'} />
 
-              <div id={`collapse${'brands'}`} className={'collapse show'} aria-labelledby={`heading${'brands'}`}>
-                <div className='card-body'>
-                  {
-                    this.props.shop.brands.map((brand, index) => (
-                      <a
-                        className='d-flex align-items-center justify-content-start'
-                        href={onFilterLinkClick({
-                          filter: 'brands',
-                          filterValue: brand.name,
-                          multiple: true,
-                          location: this.props.location
-                        })}
-                        key={brand._id}
-                        htmlFor={brand._id}
-                        style={{ cursor: 'pointer' }}>
-
-                        <input
-                          type='checkbox'
-                          id={brand._id}
-                          brand={brand.name}
-                          className='mr-2 mt-1'
-                          style={{ cursor: 'pointer', width: 18, height: 18, pointerEvents: 'none' }}
-                          checked={this.props.location.search.includes(`brands=${brand.name.split(' ').join('+')}`)} />
-
-                        <span className='text-black'>
-                          {`${brand.name} (${brand.count})`}
-                        </span>
-                      </a>
-                    ))
-                  }
-                </div>
-              </div>
+              <AccordionContent
+                show={true}
+                items={this.props.shop.brands}
+                key={'brands'}
+                location={this.props.location}
+                itemFilterKey={'brands'}
+                itemFilterValue={'name'}
+                itemKey={'_id'}
+                itemCount={'count'}
+              />
             </div>
 
             <Specifications
