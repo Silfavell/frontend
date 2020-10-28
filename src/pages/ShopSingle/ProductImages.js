fbb6a4d4eb8bd307b2f0218da262342cc16166f2
Recep Pekçetin <receppek38@gmail.com>
2020-10-28 16:55:41 +0300

    change_image_size

--- src/pages/ShopSingle/ProductImages.js
+++ src/pages/ShopSingle/ProductImages.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from 'react'
 import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io'
-import { Carousel } from 'react-bootstrap'
+import { Carousel, Media } from 'react-bootstrap'
 
 function ProductImages({ _id, images }) {
     const [index, setIndex] = useState(0)
@@ -26,9 +26,10 @@ function ProductImages({ _id, images }) {
             {
                 images.map((image) => (
                     <Carousel.Item key={image}>
-                        <div style={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
+                        <div style={{ display: 'flex', marginRight:'10vh',width: '100%', justifyContent: 'center' }}>
                             <img
                                 src={image}
+                                style={{minWidth:'384px',height:'455.4px'}}
                                 alt=''
                                 onError={(event) => {
                                     event.target.src = process.env.PUBLIC_URL + '/empty-image.webp'
